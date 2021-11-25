3b25a59df8642a5a9bde30519f20901adbc2baf0
Kathereen <duodektill@ukr.net>
2021-11-25 03:26:16 +0200

    new updates 2

--- src/index.js
+++ src/index.js
@@ -2,25 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-const initialState = 5;
-const reducer = (state = initialState, action) => {
-    switch(action.type){
-      case 'INC':
-        return state+1;
-      default:
-        return state;
-    }
-}
-const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store ={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
